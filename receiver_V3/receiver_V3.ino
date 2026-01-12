90cff3af592cb782dd28b3e65fa31baab963d5e3
Bjoshua25 <bolajijoshua35@gmail.com>
2026-01-12 10:32:31 +0100

    Include the Version 3 firmwares

---
+++ receiver_V3/receiver_V3.ino
@@ -0,0 +1,122 @@
+#include <SoftwareSerial.h>
+#include <avr/wdt.h>
+
+
+// instantiate lora, Rx and Tx
+SoftwareSerial lora (10, 11);
+
+
+// Define indicator Variables
+int lowLevelIndicator = 2;
+int middleLevelIndicator = 3;
+int highLevelIndicator = 4;
+int pumpStatus = 5;
+
+// Define button pins
+int manualButtonOn = 6;
+int manualButtonStop = 7;
+
+// Define relay pin
+int relayPin = 8;
+
+// Debounce variables
+unsigned long lastDebounceTimeOn = 0;
+unsigned long lastDebounceTimeOff = 0;
+unsigned long debounceDelay = 200; 
+bool lastOnState = HIGH;
+bool lastOffState = HIGH;
+
+
+void setup() {
+  // Disable watch dog timer at first to prevent boot loops
+  wdt_disable();
+
+
+  // Set up Serial monitor
+  Serial.begin(9600);
+  Serial.println("Pump Status");
+
+  // Set up lora Serial
+  lora.begin(9600);
+
+  // Configure Pin Modes
+  pinMode(relayPin, OUTPUT);
+  pinMode(manualButtonOn, INPUT_PULLUP);
+  pinMode(manualButtonStop, INPUT_PULLUP);
+
+  // Configure indicators
+  pinMode(lowLevelIndicator, OUTPUT);
+  pinMode(middleLevelIndicator, OUTPUT);
+  pinMode(highLevelIndicator, OUTPUT);
+  pinMode(pumpStatus, OUTPUT);
+  
+
+  // Enable Watch Dog timer after 8 seconds of inactiveness
+  wdt_enable(WDTO_8S);
+}
+
+
+
+void loop() {
+  // Reset the watch dog timer
+  wdt_reset();
+
+
+  // ========= Get watar level update, and trigger control =======
+  if (lora.available()) {
+    String receivedMessage = lora.readStringUntil("\n");
+    
+    if (receivedMessage == "ON_PUMP") {
+      digitalWrite(relayPin, HIGH);
+      digitalWrite(lowLevelIndicator, HIGH);
+      digitalWrite(middleLevelIndicator, LOW);
+      digitalWrite(highLevelIndicator, LOW);
+      digitalWrite(pumpStatus, HIGH);
+      lora.println("ON");
+      Serial.println("<50% | PUMP ON");
+    }
+
+    else if (receivedMessage == "OFF_PUMP") {
+      digitalWrite(relayPin, LOW);
+      digitalWrite(lowLevelIndicator, LOW);
+      digitalWrite(middleLevelIndicator, LOW);
+      digitalWrite(highLevelIndicator, HIGH);
+      digitalWrite(pumpStatus, LOW);
+      lora.println("OFF");
+      Serial.println("100% | PUMP OFF");
+    }
+
+    else {
+      digitalWrite(lowLevelIndicator, LOW);
+      digitalWrite(middleLevelIndicator, HIGH);
+      digitalWrite(highLevelIndicator, LOW);
+      lora.println("Approx_50");
+      Serial.println("Approx. 50%");
+    }
+  }
+
+
+
+  // ======== Manual Button Controls =======
+  bool onButtonState = digitalRead(manualButtonOn);
+  bool offButtonState = digitalRead(manualButtonStop);
+
+  if (onButtonState == LOW && lastOnState == HIGH && (millis()-lastDebounceTimeOn) > debounceDelay){
+    lastDebounceTimeOn = millis();
+    digitalWrite(relayPin, HIGH);
+    digitalWrite(pumpStatus, HIGH);
+    lora.println("ON");
+    Serial.println("Pump Manually ON");
+  }
+  lastOnState = onButtonState;
+
+
+  if (offButtonState == LOW && lastOffState == HIGH && (millis() - lastDebounceTimeOff) > debounceDelay) {
+    lastDebounceTimeOff = millis();
+    digitalWrite(relayPin, LOW);
+    digitalWrite(pumpStatus, LOW);
+    lora.println("OFF");
+    Serial.println("Pump Manually OFF");
+  }
+  lastOffState = offButtonState;
+}
---
+++ transmitter_V3/transmitter_V3.ino
@@ -0,0 +1,150 @@
+#include <SoftwareSerial.h>
+#include <avr/wdt.h>
+
+// instantiate LoRa for Rx, Tx pins
+SoftwareSerial lora(10, 11);
+
+// Define Variables
+int lowLevelPin = 2;
+int highLevelPin = 3;
+int pumpIndicator = 6;
+
+// Heartbeat Interval Variables
+unsigned long lastHeartbeat = 0;
+const unsigned long heartbeatInterval = 900000;
+
+// message variables
+String message;
+String lastMessage = "";
+
+void setup() {
+  // Always Disable WDT at first to prevent Boot Loops
+  wdt_disable(); 
+
+
+  // Set up the Serial Monitor
+  Serial.begin(9600);
+  Serial.println("Pumping Status...");
+
+  // Set up Loral Serial
+  lora.begin(9600);
+
+  // Pins Configuration
+  pinMode(lowLevelPin, INPUT_PULLUP);
+  pinMode(highLevelPin, INPUT_PULLUP);
+  pinMode(pumpIndicator, OUTPUT);
+
+
+  // Enable watch dog timer for 8 seconds
+  wdt_enable(WDTO_8S);
+}
+
+
+
+void loop() {
+  // Reset watch dog timer
+  wdt_reset();
+
+
+  // ================= Sense the water level and send state =======================
+  // check the water level
+  bool lowLevelStatus = digitalRead(lowLevelPin);
+  bool highLevelStatus = digitalRead(highLevelPin);
+
+  // Logic pumping Control
+  if (lowLevelStatus == LOW){
+    message = "ON_PUMP";
+  }
+
+
+  else if (highLevelStatus == HIGH){
+    message = "OFF_PUMP";
+  }
+
+
+  else {
+    message = "Approx. 50%";
+  }
+
+
+
+
+
+
+  // ====================== Transmission and Acknowledgement Logic ==================
+  if (message != lastMessage){
+    bool acknowledged = false;
+    int retries = 0;
+
+    // Transmit to Controller if retry less than 3
+    while (!acknowledged && retries < 3){
+      wdt_reset(); 
+
+      lora.print(message);
+      Serial.print("Sending: "); Serial.println(message);
+      Serial.print(" (Attempt "); Serial.print(retries + 1); Serial.println(")");
+
+      // Receive acknowledgement from controller
+      unsigned long startWait = millis();
+      while (millis() - startWait < 2000){
+        if (lora.available()){
+          String response = lora.readStringUntil("\n");
+          response.trim();
+
+          if ((message == "ON_PUMP" && response == "ON") || 
+              (message == "OFF_PUMP" && response == "OFF") ||
+              (message == "Approx. 50%" && response == "Approx_50")){
+                acknowledged = true;
+                Serial.println("ACK Received!");
+                break;
+          }
+        }
+      }
+
+
+      // if There is no Acknowldgement; Update No of Retries
+      if (!acknowledged){
+        retries++;
+        Serial.println("No ACK, retrying...");
+        delay(100);
+      }
+    }
+
+    if (!acknowledged) Serial.println(("Error: No ACK after 3 attempts."));
+    lastMessage = message;
+  }
+
+
+
+
+
+
+
+  // ====================== Receive Pump state from site ======================
+  if (lora.available()) {
+    String pumpState = lora.readStringUntil("\n");
+    pumpState.trim();
+
+    if (pumpState == "ON"){
+      digitalWrite(pumpIndicator, HIGH);
+      Serial.println("Received ON command");
+    }
+
+    else if (pumpState == "OFF") {
+      digitalWrite(pumpIndicator, LOW);
+      Serial.println("Received OFF command");
+    }
+  }
+
+  delay(200);
+
+
+  // ============ Periodic Heart Beat Logic =================================
+  if (millis() - lastHeartbeat > heartbeatInterval) {
+    lastMessage = "";
+    lastHeartbeat = millis();
+  }
+}
+
+
+
